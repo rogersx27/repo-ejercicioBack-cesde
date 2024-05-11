8fe78b935aaaf6df7c854206a97b0e1eb62ec3f2
jaimezzapata <zapataval2304@gmail.com>
2024-05-11 14:19:42 -0500

    hu458 feat se configura capa de modelos

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import org.example.modelos.Vehiculo;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //DENTRO DEL MAIN USO LAS CLASES DEL PROGRAMA
+
+        //COMO SE USA UNA CLASE?
+        //(USAR LA CLASE ES ASIGNARLE VALORES A SUS ATRIBUTOS)
+        //(USAR LA CLASE ES LLAMAR SUS METODOS)
+
+        //PARA USAR UNA CLASE DEBO CREAR UN OBJETO
+
+        //QUE ES UN OBJETO?
+        //UN OBJETO ES UNA VARIABLES PERO ESPECIAL
+        Integer edad=34;
+        Vehiculo objeto;
+
+    }
+}
---
+++ src/main/java/org/example/modelos/Mercancia.java
@@ -0,0 +1,14 @@
+package org.example.modelos;
+
+public class Mercancia {
+
+    //id (int)
+    //volumenOcupado (double)
+    //tipo (string)
+    //peso (double)
+    //descripcion (string)
+    //avaluo (int)
+    //direccionRemitente (string)
+    //direccionDestinatario (string)
+
+}
